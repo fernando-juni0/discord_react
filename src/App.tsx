bbe4fce0fe4960f14ba4ebd0111152c62de23615
Fernando <junio132sj@gmail.com>
2023-04-14 11:25:10 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import GlobalStyles from './css/GlobalStyles';
+
+
+function App() {
+  return (
+      <>
+        <h1>Test</h1>
+        <GlobalStyles />
+      </>
+  );
+}
+
+export default App;
